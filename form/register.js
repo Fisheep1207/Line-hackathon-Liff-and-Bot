4cba76bed4f0df375580b92dabcfd4c9c01679b9
Fisheep Chiou <fisheepchiou@TW-LW81598-M.local>
2020-09-24 11:27:19 +0800

    form modify

--- form/register.js
+++ form/register.js
@@ -1,50 +1,40 @@
-var vConsole = new VConsole();
-console.log("in js");
-async function initializeLiff(myLiffId) {
-        console.log(myLiffId);
-        try{
-            return liff.init({liffId: myLiffId});
-        }
-        catch(err){
-            console.log(err);
-        }
-}
 const form = document.getElementById('theform')
 form.addEventListener('submit', async (e) => {
     console.log("Hello");
-    e.preventDefault();
+    // e.preventDefault();
     let form_data = new FormData(form);
-    // console.log("form_data = ", form_data);
-    let tmp = await initializeLiff('1654947700-lGNQ8emK');
-    let baseURL = `https://26503cf3fa8d.ngrok.io/receiveMessege`;
-    var profile;
-    try{
-        profile = await liff.getProfile();
-        console.log(profile);     
-    }
-    catch(err){
-        console.log("err = ", err);
-    }
-    form_data.append("userId", profile.userId);
+    let baseURL = `http://127.0.0.1:30000/p`;
+    form_data.append("userId", "Fuckabro");
     var object = {};
     form_data.forEach(function(value, key){
-        object[key] = value;
+        // console.log("key = ", key, "value = ", value);
+        if (key in object && Array.isArray(object[key])){
+            object[key].push(value);
+        }
+        else if (key in object){
+            object[key] = [object[key], value];
+        }
+        else {
+            object[key] = value;
+        }
     });
     console.log(object);
     let json_data = JSON.stringify(object);
     console.log(json_data);
-    try{
-        let res = await fetch(baseURL, {
-        method:'POST',
-        body:  json_data,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-        })
-    }
-    catch(err){
-        console.log(err);
-    }
+    // try{
+    //     let res = await fetch(baseURL, {
+    //     method:'POST',
+    //     body:  json_data,
+    //     headers: {
+    //         'Content-Type': 'application/json'
+            
+    //     }
+    //     })
+    //     console.log(res);
+    // }
+    // catch(err){
+    //     console.log(err);
+    // }
 });
 
 // ~async function(e){
@@ -53,3 +43,5 @@ form.addEventListener('submit', async (e) => {
 //         console.log("WTF");
 //     })
 // }()
+
+
